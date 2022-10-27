b6b2b4e009927be2e909410f5b3d192628e14b28
esteres <esteban.desarrollador@gmail.com>
2022-10-27 08:04:12 -0500

    Fix user tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
+  belongs_to :user
 
+  def body_preview
+    body.truncate(15, separator: /\n/)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  has_many :posts
 
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ 
+
+  def email_preview
+    email.gsub(/@.*$/, '@*******.com')
+  end
 end
